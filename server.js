c72b7feddda1a11c610d58287f69e383779bce17
maarten <van_den_bergmaarten@hotmail.com>
2023-11-25 10:44:38 +0100

    build lesson

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express = require("express");
+const fs = require("fs").promises; // Use fs.promises for asynchronous file reading
 
 const app = express();
-app.listen(3000, () => console.log("listening at 3000"));
-app.use(express.static("public"));
-app.use(express.json({ limit: "1mb" }));
+const port = 3000;
+
+app.get("/lesson/:lessonId", async (req, res) => {
+  try {
+    const lessonId = req.params.lessonId;
+
+    // Read JSON file
+    const jsonFilePath = `files/${lessonId}.json`;
+    const lessonData = JSON.parse(await fs.readFile(jsonFilePath, "utf8"));
+
+    // Read Solidity file
+    const solidityFilePath = `solidity_files/${lessonId}.sol`;
+    const solidityCode = await fs.readFile(solidityFilePath, "utf8");
+
+    // Combine data
+    lessonData.solidityCode = solidityCode;
+
+    res.json(lessonData);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
